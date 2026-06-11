9f83a0597543438d9a3fc0bdc40856d08a88a1fc
DavyDW <42499767+davydw@users.noreply.github.com>
2026-06-11 10:11:05 +0800

    feat(math):added a new parameter

--- Module02_DataToolkit/03-CodingProperly/00-Lecture/supermath/fancy_mathematics.py
+++ Module02_DataToolkit/03-CodingProperly/00-Lecture/supermath/fancy_mathematics.py
@@ -1,5 +1,5 @@
 def sum_of_three_numbers(a, b, c):
     return a + b + c
 
-def product_of_three_numbers(a,b,c):
-    return a * b * c
+def product_of_three_numbers(a,b,c,d):
+    return a * b * c * d
